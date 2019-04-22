43989eb63fe4f8d4e393676ec08fcf2676f8df72
l1707823672 <47018957+l1707823672@users.noreply.github.com>
2019-04-22 23:50:55 +0800

    mult

---
+++ js/mult/map.js
@@ -0,0 +1,41 @@
+// 对象 林 
+const googleMap = {
+    show() {
+        console.log('开始渲染GOOGLE地图');
+    }
+}
+const gaodeMap = {
+    show() {
+        console.log('开始渲染高德地图');
+    }
+}
+const baiduMap = {
+    // 为互换作准备
+    show(){
+        console.log('开始渲染百度地图');
+    }
+}
+const sosoMap={}
+/**
+ * 
+ * @param {object} type 
+ */
+const renderMap = (map) => {
+    // map  检测
+    if(map.show && typeof map.show==='function'){
+        map.show();
+    }
+    
+    // googleMap.show();
+    // 分支太多
+    // if(type==='baidu')
+    //     baiduMap.show();
+    // else if(type==='google')
+    // googleMap.show();
+    // else if(type==='gaode')
+    // gaodeMap.show();
+}
+
+renderMap(baiduMap);
+renderMap(googleMap);
+renderMap(sosoMap);
