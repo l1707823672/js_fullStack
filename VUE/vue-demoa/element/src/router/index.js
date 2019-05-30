67772cf36f11bffeb13281b47886ddd148eb65bc
l1707823672 <1707823672@qq.com>
2019-05-30 23:59:25 +0800

    h后台管理

--- VUE/vue-demoa/element/src/router/index.js
+++ VUE/vue-demoa/element/src/router/index.js
@@ -1,6 +1,9 @@
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -8,8 +11,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path:'/main',
+      name:'Main',
+      component:Main
     }
   ]
 })
+
