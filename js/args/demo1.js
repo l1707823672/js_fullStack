b6d613b93a09ce979f8300e73c62a03ed801f0a9
l1707823672 <1707823672@qq.com>
2019-07-07 23:14:16 +0800

    args

---
+++ js/args/demo1.js
@@ -0,0 +1,6 @@
+function information(name,sex,height,birthday,color,likes,follow,age){
+    console.log(name,sex,height,birthday,color,likes,follow,age);
+}
+let user={"name":'',"sex":'',"height":'',"birthday":'',"color":'',"likes":'',"follow":'',"age":''}
+information('熊云松','男',178,'7-13','黄','...','',18)
+information(user);
---
+++ js/args/demo4.js
@@ -0,0 +1,10 @@
+const err=(message)=>{
+    throw new Error(message);
+}
+function sum(num= err('first param is not defined'),otehrNum= err('second param is not defined')){
+    return sum;
+}
+console.log(underfind,10);
+// 报错 第一个参数没有定义
+console.log(10);
+// 报错，第二个参数没有定义
