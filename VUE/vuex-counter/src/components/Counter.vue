7301626ca7421006551eec25a7dfa5f3596d29b4
l1707823672 <1707823672@qq.com>
2019-07-08 09:22:30 +0800

    counter

---
+++ VUE/vuex-counter/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{$store.state.count}}  times, 
+    <button 
+    @click="increment">+</button>
+  </div>
+</template>
+<script>
+export default {
+  props: ['count'],
+  methods: {
+    increment () {
+     
+    }
+  }
+}
+</script>
+<style>
+</style>
