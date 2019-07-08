2e6c3ac52f17dcdd3d4a567a7d692d5dd32832be
l1707823672 <1707823672@qq.com>
2019-07-08 09:07:09 +0800

    do_vuex_counter

---
+++ VUE/do_vuex_counter/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    {{$store.state.count}}  time,count is 
+    {{evenOrOdd}}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">incrementAsync</button>
+  </div>
+</template>
+
+<script>
+import {mapActions,mapGetters} from 'vuex';
+export default {
+  name: 'App',
+  computed: mapGetters(
+    ['evenOrOdd']
+  ),
+  methods: 
+    mapActions([
+      'increment',
+      'decrement',
+      'incrementIfOdd',
+      'incrementAsync'
+    ]),
+  
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
