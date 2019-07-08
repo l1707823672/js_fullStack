ad065133a27cbe86dfd38fd960a2c79dae1e4a3d
l1707823672 <1707823672@qq.com>
2019-07-08 09:01:16 +0800

    smzdm-youhui

---
+++ node/smzdm-youhui/index.js
@@ -0,0 +1,21 @@
+const request=require('request');
+const cheerio=require('cheerio');
+request('https//www.smzdm.com/youhui/',(err,res)=>{
+    if(!err){
+        console.log(res.body);
+        // decodeEntities 要不要解析 html entity 
+        const $ = cheerio.load(res.body,{
+            decodeEntities:false
+        })
+        $('.list.list_preferential').each(function(){
+            // 限制
+            const title=$('.itemName a',this).html();
+            title=title.replace(/<.*>.*<\/.*>/g,'')
+            const price = $('.listTitle.red',this).text()
+            const img=$('.picLeft img').attr('src');
+            const desc=$('.lrInfo',this).text().trim();
+            console.log({
+                title,price,img,desc});
+        })
+    }
+})
